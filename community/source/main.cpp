4b00aacf83354819ee78b10563824beab855b57a
Denactive <d.turchin@bk.ru>
2021-04-14 16:48:13 +0300

    work begin

---
+++ community/source/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by denactive on 14.04.2021.
+//
+
+int main() {
+    Options opts("SaberLion-Devs");
+    Server s(opts);
+    s.run();
+    return 0;
+}
---
+++ server/source/server.h
@@ -0,0 +1,17 @@
+//
+// Created by denactive on 14.04.2021.
+//
+
+#ifndef CHECKMATES_SERVER_H
+#define CHECKMATES_SERVER_H
+
+class Options {
+    
+};
+
+class Server {
+
+};
+
+
+#endif //CHECKMATES_SERVER_H
