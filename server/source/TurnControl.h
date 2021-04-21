984442cc4ed635532978a8bf24161c370bf4e4a5
yura11011 <yura11011@yandex.ru>
2021-04-21 23:46:47 +0300

    congig added. privacy fixed

--- server/source/TurnControl.h
+++ server/source/TurnControl.h
@@ -7,26 +7,27 @@
 #include "GameSession.h"
 
 class iTurnControl {
-public:
-    virtual ~iTurnControl() { };
+private:
     virtual void SwitchTurn() = 0;
     virtual void StopTimer() = 0;
     virtual void EnableTimer() = 0;
+public:
+    virtual ~iTurnControl() { };
     virtual time_t GetTime(bool Turn) = 0;
     virtual bool GetTurn(void) = 0;
 
 };
 
 class TurnControl: public iTurnControl {
-public:
-    ~TurnControl();
+private:
     bool Turn;
     time_t Timer1;
     time_t Timer2;
     void SwitchTurn();
     void EnableTimer();
     void StopTimer();
-
+public:
+    ~TurnControl();
     time_t GetTime(const bool Turn);
     bool GetTurn(void);
 };
