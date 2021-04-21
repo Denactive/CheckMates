5047817f2d0d57885a67f043557c8137e7af37d0
yura11011 <yura11011@yandex.ru>
2021-04-21 21:35:16 +0300

    test1?

--- server/source/GameSession.h
+++ server/source/GameSession.h
@@ -19,25 +19,27 @@ public:
     virtual void GiveUpHandler() = 0;
     virtual void StalemateHandler() = 0;
     virtual void CreateLog() = 0;
+    iTurnControl* control;
 };
 
 class GameSession: public iGameSession {
 public:
-    GameSession(iTurnControl& l):control(l){}
+    GameSession(iTurnControl* l):control(l){}
     void run();
     std::string GetStatus();
     void DrawHandler();
     void GiveUpHandler();
     void StalemateHandler();
     void CreateLog();
-    iTurnControl& control;
+    iTurnControl* control;
 private:
-    BDServer log;
-    ChessBoard board;
-    Player wPlayer;
-    Player bPlayer;
-    std::string status;
-    TurnHistory history;
+
+   // BDServer log;
+   // ChessBoard board;
+   // Player wPlayer;
+   // Player bPlayer;
+    //std::string status;
+    //TurnHistory history;
 
 };
 
--- server/test/test1.cpp
+++ server/test/test1.cpp
@@ -3,23 +3,31 @@
 #include "GameSession.h"
 
 
-
-class reTurnControl {
+class reTurnControl: public iTurnControl {
+private:
+    bool Turn;
+    time_t Timer1;
+    time_t Timer2;
+    void SwitchTurn();
+    void EnableTimer();
+    virtual void StopTimer();
 public:
     MOCK_METHOD(time_t, GetTime, (bool Turn) );
-    MOCK_METHOD(bool, GetTurn, () );
+    MOCK_METHOD(bool, GetTurn, ());
 };
+
 
 using ::testing::Return;
 TEST(gamesession, TurnControl) {
     reTurnControl control;
-    iTurnControl* c = (iTurnControl*)&control;
-    GameSession Session(*(c));
+    iTurnControl* c = &control;
+    GameSession Session(c);
     ON_CALL(control, GetTime(true))
             .WillByDefault(Return(100));
     ON_CALL(control, GetTurn())
               .WillByDefault(Return(true));
-    EXPECT_EQ(Session.control.GetTime(Session.control.GetTurn()), 100);
+
+    EXPECT_EQ(Session.control->GetTime(Session.control->GetTurn()), 100);
 
 }
 
