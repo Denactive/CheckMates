e54226de82b0f350b24c554cf3aa1b72a2f59b60
yura11011 <yura11011@yandex.ru>
2021-04-22 13:41:25 +0300

    different MACROSS

--- server/test/test1.cpp
+++ server/test/test1.cpp
@@ -13,16 +13,16 @@ TEST(gamesession, Mock) {
     #include "OnCallTurn.h"
     BDServer log;
     GameSession session(&log, &control, &player);
-    EXPECT_EQ(session.wPlayer->try_move(), true);
-    EXPECT_EQ(session.wPlayer->is_check(), false);
-    EXPECT_EQ(session.wPlayer->is_mate(), false);
-    EXPECT_EQ(session.wPlayer->is_stalemate(), false);
+    EXPECT_TRUE(session.wPlayer->try_move());
+    EXPECT_FALSE(session.wPlayer->is_check());
+    EXPECT_FALSE(session.wPlayer->is_mate());
+    EXPECT_FALSE(session.wPlayer->is_stalemate());
     EXPECT_EQ(session.wPlayer->all_available_Moves(), vec);
     EXPECT_EQ(session.wPlayer->GetUserId(), 0);
-    EXPECT_EQ(session.control->GetTurn(), true);
+    EXPECT_TRUE(session.control->GetTurn());
     EXPECT_EQ(session.control->GetTime(true), 100);
     EXPECT_EQ(session.GetTime(), 100);
-    EXPECT_EQ(session.MakeMove(), true);
+    EXPECT_TRUE(session.MakeMove());
     EXPECT_EQ(session.wPlayer->GetUserId(), 0);
     EXPECT_EQ(session.log->query("new"), "new");
 }
