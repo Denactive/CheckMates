d78ae285048af5167197c5af8f31a8bdc14f5311
yura11011 <yura11011@yandex.ru>
2021-04-24 14:33:08 +0300

    newchess

--- server/source/Player.h
+++ server/source/Player.h
@@ -1,10 +1,8 @@
-#pragma once
 
-#include "GameSession.h"
+#pragma once
 #include "User.h"
 
 class iPlayer {
-
 public:
     virtual std::vector<std::string> all_available_Moves() = 0;
     virtual bool try_move() = 0;
@@ -19,20 +17,15 @@ class Player: public iPlayer {
 private:
     ChessBoard& board;
     User user;
-
 public:
-    Player(ChessBoard& board, ChessPiece* pieces):  board(board),  pieces(pieces) {};
-    ChessPiece* pieces;
-    std::vector<std::string> all_available_Moves() {
-        std::vector<std::string> vec;
-        return vec;
-    };
-
+    Player(ChessBoard& board, std::vector<ChessPiece*> pieces);
+    std::vector<ChessPiece*> pieces;
+    std::vector<std::string> all_available_Moves();
     bool try_move() {return true;};
-    bool is_check(){return true;};
-    bool is_mate(){return true;};
-    bool is_stalemate(){return true;};
-    size_t GetUserId(){return 0;};
+    bool is_check();
+    bool is_mate();
+    bool is_stalemate();
+    size_t GetUserId();
     void set_pieces(){};
 };
 
