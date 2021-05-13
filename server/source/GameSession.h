e6d6101f9af5768aed3c5775c366f1e03c626993
yura11011 <yura11011@yandex.ru>
2021-05-13 15:57:21 +0300

    hybrid version

--- server/source/GameSession.h
+++ server/source/GameSession.h
@@ -12,26 +12,27 @@
 #include "Player.h"
 #include "TurnControl.h"
 
-struct GInfo{
+struct GInfo {
     bool isPlayer;
     bool isGame;
     char isVictory;
     bool isCheck;
+    std::array<size_t, M> turn;
 };
 
 class IGameSession {
 public:
     virtual ~IGameSession() {};
     virtual void CreateLog() = 0;
-    virtual std::array<size_t, M> GetTurn() = 0;
     virtual time_t GetTime() = 0;
     virtual void run() = 0;
-    virtual int run_turn() = 0;;
+    virtual int run_turn(std::array<size_t, M> turn) = 0;
+    virtual void setup() = 0;
     virtual bool is_check(class IPlayer* you, IPlayer* enemy) = 0;
     virtual bool is_mate(IPlayer* you, IPlayer* enemy)= 0;
     virtual bool is_stalemate(IPlayer* you, IPlayer* enemy)= 0;
     virtual void send_move(std::array<size_t, M> turn) = 0;
-    virtual void send_info(GInfo info) = 0;
+    virtual GInfo send_info() = 0;
 };
 
 class GameSession: public IGameSession {
@@ -46,17 +47,18 @@ public:
     bool is_stalemate(IPlayer* you, IPlayer* enemy);
     bool GameStatus();
     void run();
-    int run_turn();
+    int run_turn(std::array<size_t, M> turn);
+    void setup();
     time_t GetTime();
     ~GameSession() = default;
-    std::array<size_t, M> GetTurn();
     void print_moves (IPlayer* you);
     void move(IPlayer* you, IPlayer* enemy, std::array<size_t, M> turn);
     void send_move(std::array<size_t, M> turn) {
         std::cout <<"\n"<< turn[0] << turn[1] << turn[2] << turn[3]<<"\n";
     }
-    void send_info(GInfo info) {
+    GInfo send_info() {
         std::cout <<"\n"<< info.isPlayer << info.isGame << info.isVictory << info.isCheck <<"\n";
+        return info;
     }
     void try_move(IPlayer* you, IPlayer* enemy);
    // IDBServer* log;
--- server/source/main.cpp
+++ server/source/main.cpp
@@ -1,15 +1,34 @@
 
 #include "server.h"
 
+std::array<size_t, M> GetTurn() {
+    std::array<size_t, M> turn;
+    std::cin >> turn[0] >>  turn[1] >> turn[2] >> turn[3];
+    return turn;
+}
+
 int main() {
     // BE CAREFUL WITH FILEPATH
+
+
     ChessBoard board;
     board.set_board();
     Player player1(board, true);
     Player player2(board, false);
     TurnControl control;
     GameSession A(control, player1, player2);
-    A.run();
+    //A.run();
+
+    A.setup();
+    GInfo info = A.send_info();
+    std::array<size_t, M> turn;
+    while (info.isGame) {
+        turn = GetTurn();
+        std::cout << '@';
+        int turn_accepted = A.run_turn(turn);
+        info = A.send_info();
+    }
+    std::cout << '*';
 
     Options opts("SaberLion-Devs", "127.0.0.1", 8000, 8001, "C:/Users/Denactive/source/repos/SaberDevs_CheckMates/server/storage");
     Server s(opts);
