490a4fd42134a791351559a66d688b4763680771
yura11011 <yura11011@yandex.ru>
2021-06-06 20:03:13 +0300

    start work

--- server/test/rePlayer.h
+++ server/test/rePlayer.h
@@ -1,10 +1,29 @@
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <vector>
+
+
+/*virtual std::shared_ptr<WebSocketSession> get_session() = 0;
+virtual void set_session(std::shared_ptr<WebSocketSession>) = 0;
+virtual std::vector<std::array<size_t, M>>  all_available_Moves() = 0;
+virtual void move(std::array<size_t, M> turn) = 0;
+virtual void is_captured(std::array<size_t, M> turn) = 0;
+virtual size_t try_capture(std::array<size_t, M> turn) = 0;
+virtual std::shared_ptr<IUser> get_user() = 0;
+virtual void set_pieces() = 0;
+virtual void flag_castl(std::array<size_t, M> turn) = 0;
+virtual void print_pos() = 0;
+virtual std::shared_ptr<ChessBoard> getboard () = 0;
+virtual void KingUpdate(std::set<std::array<size_t, K>> thr) = 0;
+virtual const size_t* where() = 0;
+virtual std::vector<std::array<size_t, M>>& access() = 0;
+virtual std::set<std::array<size_t, K>> all_threatens() = 0;
+ */
 class rePlayer: public IPlayer {
 public:
-    MOCK_METHOD(std::vector<std::string>, all_available_Moves, ());
-    MOCK_METHOD(bool, try_move, ());
-    MOCK_METHOD(bool, is_check, ());
-    MOCK_METHOD(bool, is_mate, ());
-    MOCK_METHOD(bool, is_stalemate, ());
-    MOCK_METHOD(void, set_pieces, ());
-    MOCK_METHOD(size_t, GetUserId, ());
+    MOCK_METHOD((std::vector<std::array<size_t, 4>>), all_available_Moves, (), (override));
+    MOCK_METHOD(void, set_session, (std::shared_ptr<WebSocketSession>), (override));
+    MOCK_METHOD((void), move, (std::array<size_t, 4>), (override));
+    MOCK_METHOD(std::shared_ptr<WebSocketSession>, get_session, (), (override));
 };
--- server/test/test1.cpp
+++ server/test/test1.cpp
@@ -7,8 +7,23 @@
 using ::testing::Return;
 using ::testing::AtLeast;
 
+
 TEST(gamesession, Mock) {
-    #include "OnCallPlayer.h"
+    rePlayer player;
+    ON_CALL(player, try_move())
+            .WillByDefault(Return(true));
+    ON_CALL(player, is_check())
+            .WillByDefault(Return(false));
+    ON_CALL(player, is_mate())
+            .WillByDefault(Return(false));
+    ON_CALL(player, is_stalemate())
+            .WillByDefault(Return(false));
+    ON_CALL(player, GetUserId())
+            .WillByDefault(Return(0));
+    std::vector<std::string> vec;
+    vec.push_back("new");
+    ON_CALL(player, all_available_Moves())
+            .WillByDefault(Return(vec));
     #include "OnCallTurn.h"
     DBServer log;
     GameSession session(&log, &control, &player);
