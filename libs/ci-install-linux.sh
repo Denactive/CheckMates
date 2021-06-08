3260fe3dceb5b59c8b5ca82aa7711eb0f9c5517d
Denactive <d.turchin@bk.ru>
2021-06-08 10:16:38 +0300

    travis-13

---
+++ libs/ci-install-linux.sh
@@ -0,0 +1,43 @@
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+
+sudo apt-get install g++
+sudo apt-get install cmake
+
+#sudo apt-get install libgtest-dev
+##cd /usr/src/gtest
+#sudo cmake CMakeLists.txt
+#sudo make
+#sudo cp ./lib/libgtest*.a /usr/lib
+#cd -
+
+#gmock
+#sudo apt-get install libgmock-dev
+curl -O https://github.com/google/googletest/archive/refs/heads/master.zip
+#ls -lh /home/travis/Downloads 
+#sudo mv /home/travis/Downloads/googletest-master.zip /usr/src/
+#sudo unzip /usr/src/googletest-master.zip
+#sudo mv /usr/src/googletest-master /usr/src/googletest
+#ls -lh /usr/src/googletest
+#sudo mkdir /usr/src/googletest/googlemock/build
+#sudo cd /usr/src/googletest/googlemock/
+#sudo cmake ..
+#sudo make
+#sudo cp *.a /usr/lib
+#cd /home/travis/
+#ls -lh
+sudo cp libs/*.a /usr/local/bin
+
+
+
+- pip install codecov
+#sudo apt-get install -y valgrind
+#sudo apt-get install -y cppcheck
+
+#gcovr
+#Installing python is unnesessary
+#sudo apt install -y python3-pip
+sudo apt-get install python3-setuptools && sudo pip3 install git+https://github.com/gcovr/gcovr.git
+
+#lcov
+sudo apt-get install lcov
